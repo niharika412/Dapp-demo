e5fcb17a646df51b437fb08517da85e43b8d3e43
Niharika Pandit <niharikap219@gmail.com>
2022-01-12 20:44:23 +0530

    dev: created function to add new books

--- eth-bookrateWS/contracts/BookRater.sol
+++ eth-bookrateWS/contracts/BookRater.sol
@@ -1,5 +1,22 @@
 pragma solidity >=0.5.16;
 
 contract BookRater {
-  uint public taskCount = 0;
+    uint256 public taskCount = 0;
+
+    struct Rater {
+        uint256 id;
+        string content;
+        bool rating;
+    }
+
+    mapping(uint256 => Rater) public ratedBooks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        ratedBooks[taskCount] = Rater(taskCount, _content, false);
+    }
+
+    constructor() public{
+        createTask("Emma");
+      }
 }

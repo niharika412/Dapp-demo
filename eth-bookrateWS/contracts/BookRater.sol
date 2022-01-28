ec3ff317d70e9f8dc1a3d4ddceabaf1f8cef262e
Niharika Pandit <niharikap219@gmail.com>
2022-01-28 21:06:31 +0530

    feat: delete function added

--- eth-bookrateWS/contracts/BookRater.sol
+++ eth-bookrateWS/contracts/BookRater.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.5.16;
 
+//aim is to create a CRUD Ap
 contract BookRater {
     uint256 public taskCount = 0;
 
@@ -11,18 +12,39 @@ contract BookRater {
 
     mapping(uint256 => Rater) public ratedBooks;
 
-    function createTask(string memory _content, uint rating) public {
+    event BooksUpdated(string bookName, uint256 rating);
+
+    event BookDeleted(uint256 task);
+
+    //create fn
+    function createTask(string memory _content, uint256 rating) public {
         taskCount++;
         ratedBooks[taskCount] = Rater(taskCount, _content, rating);
     }
 
-    constructor() public{
-        createTask("The Seven Husbands of Evelyn Hugo",8);
-        createTask("The Vanishing Half",9);
-      }
-    
-    function changeRating(uint taskno,uint rating) public {
-        Rater storage b1 = ratedBooks[taskno];
-        b1.rating = rating;
+    constructor() public {
+        createTask("The Seven Husbands of Evelyn Hugo", 8);
+        createTask("The Vanishing Half", 9);
+    }
+
+    //update fn
+    function changeRating(uint256 taskno, uint256 rating) public {
+        if (taskno > taskCount) {
+            revert(("Invalid Book ID no"));
+        } else {
+            Rater storage b1 = ratedBooks[taskno];
+            b1.rating = rating;
+            emit BooksUpdated(b1.content, b1.rating);
+        }
+    }
+
+    //delete fn
+    function deleteRating(uint256 taskno) public {
+        if (taskno > taskCount){
+            revert("Invalid Book ID no.");
+        }else{
+            delete ratedBooks[taskno];
+            emit BookDeleted(taskno);
+        }
     }
 }
